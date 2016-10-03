4018b136a3bdb7f599bbe5503165be86004868f2
devendra14 <dhakaddevendra14@gmail.com>
2016-10-03 10:18:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.get('/:dev',function (req, res) {
 });
 
 
+app.get('/article-three',function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
+});
+
+
 
 
 app.get('/ui/style.css', function (req, res) {
