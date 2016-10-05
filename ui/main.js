e9f45181733e9e0f27bf370a08db553210179648
devendra14 <dhakaddevendra14@gmail.com>
2016-10-05 11:30:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,7 @@ button.onclick = function()
     
     //render the veriable to the currect span
     
-    counter=counter + 3;
+    counter = counter + 3;
     var span=document.getElementById('count');
     span.innerHTML = counter.toString();
     
