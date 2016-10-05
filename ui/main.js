86ad55b1ae3f89b3d6909b3b69fe1dbc4063772e
devendra14 <dhakaddevendra14@gmail.com>
2016-10-05 07:48:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,16 +3,18 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='this is a new value';
 //move image 
-var das=document.getElementById('dev');
+var das= document.getElementById('dev');
  var marginLeft=0;
-function moveRight() {
+ 
+function moveRight()
+ {
     marginLeft= marginLeft+10;
     das.style.marginLeft = marginLeft + 'px';
-}
+ }
 
 das.onclick= function()
 {
     
-  var interval = setinterval(moveRight, 10);
+  var interval = setInterval(moveRight, 10);
 
 };
