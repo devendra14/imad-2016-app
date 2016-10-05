447e957bcee7690572ba3f0b7d33113549c82086
devendra14 <dhakaddevendra14@gmail.com>
2016-10-05 08:52:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,6 +72,17 @@ var dev=req.params.articlename;
 res.send(templat(articles[dev]));
 });
 
+ var counter=0;
+app.get('/counter',function (req, res) {
+ counter=counter+1;
+ res.send(counter.toString(counter));   
+    
+    
+    
+});
+
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
