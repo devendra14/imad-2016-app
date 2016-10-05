5152a0523d09405e6cb7298f4ce4a5092a7061c6
devendra14 <dhakaddevendra14@gmail.com>
2016-10-05 11:36:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ button.onclick = function()
     
     //render the veriable to the currect span
     
-    counter = counter++;
+    counter = counter+1;
     var span=document.getElementById('count');
     span.innerHTML = counter.toString();
 };
