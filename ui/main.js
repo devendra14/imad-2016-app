e4872a55c6e8319f2ac82549086c86267aa6f402
devendra14 <dhakaddevendra14@gmail.com>
2016-10-07 14:45:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,7 +32,7 @@ submit.onclick = function()
   //make a request to the server and send the name
   var names = ['name1','name2','name3','name4'];
   var list='';
-  for(var i=0; i<name.length;   i++)
+  for(var i=0; i<names.length;   i++)
   {
       list+= '<li>' + names[i]+'</li>';
   }
