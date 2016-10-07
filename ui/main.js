00fda6762515f38b65446431137a503cc9b7b0ba
devendra14 <dhakaddevendra14@gmail.com>
2016-10-07 14:47:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,10 +38,5 @@ submit.onclick = function()
   }
   var ul = document.getElementById('namelist');
   ul.innerHTML = list;
-   
   //capture a list of name and render it as a list
-
-
-  
-    
   };
