75f2cfac8cc9d94129ffb2232255779a3dd755a4
devendra14 <dhakaddevendra14@gmail.com>
2016-10-07 16:15:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,9 +98,9 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names =[];
-app.get('/submit-name/:name',function(req, res){
+app.get('/submit-name',function(req, res){
     // get the name from the requt
-    var name = req.params.name;
+    var name = req.query.name;
 
     names.push(name);
     //JSON java script object notation
