925ee923e0ed138278fe9fc9642ec301d25043e2
devendra14 <dhakaddevendra14@gmail.com>
2016-10-09 12:12:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,16 +17,6 @@ button.onclick = function()
                var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
                
-               
-                     //make a request to the server and send the name
-              var names = ['name1','name2','name3','name4'];
-              var list='';
-              for(var i=0; i<names.length;   i++)
-                 {
-                    list+= '<li>' + names[i]+'</li>';
-                  }
-              var ul = document.getElementById('namelist');
-              ul.innerHTML = list;
            }
         }
         //not done yet
@@ -37,14 +27,21 @@ button.onclick = function()
 };
 
    
-    
-    
-
 //submit name
-//var nameInput = document.getElementById('name');
-//var name = nameInput.value;
-//var submit = document.getElementById('submit_btn');
-//submit.onclick = function()
-  //  {
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function()
+ {
+            //make a request to the server and send the name
+              var names = ['name1','name2','name3','name4'];
+              var list='';
+              for(var i=0; i<names.length;   i++)
+                 {
+                    list+= '<li>' + names[i]+'</li>';
+                  }
+              var ul = document.getElementById('namelist');
+              ul.innerHTML = list;
+     
 
-//};
+};
