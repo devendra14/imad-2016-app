4cbd503b54b8435ef476486b6117eaddf45cba54
devendra14 <dhakaddevendra14@gmail.com>
2016-10-09 12:06:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,7 @@
 //counter code
 var button= document.getElementById('counter');
-var counter=0;
 button.onclick = function()
 {
-    counter++;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-    
-    
-    
-    
-    
-    
-    
-    
     
     //creat a requst
     var request = new XMLHttpRequest();
@@ -25,25 +13,44 @@ button.onclick = function()
            //take some action
            if(request.status ===200)
            {
+               var counter =requst.responceText;
+               var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+               
                
                      //make a request to the server and send the name
-      var names = ['name1','name2','name3','name4'];
-      var list='';
-      for(var i=0; i<names.length;   i++)
-       {
-         list+= '<li>' + names[i]+'</li>';
-       }
-      var ul = document.getElementById('namelist');
-      ul.innerHTML = list;
-              
+              var names = ['name1','name2','name3','name4'];
+              var list='';
+              for(var i=0; i<names.length;   i++)
+                 {
+                    list+= '<li>' + names[i]+'</li>';
+                  }
+              var ul = document.getElementById('namelist');
+              ul.innerHTML = list;
            }
         }
         //not done yet
     };
-   //make a requst 
-   request.open('GET','http://devendra14.imad.hasura-app.io/submit?name='+name, true);
-   request.send(null);
+    //make a requst 
+   request.open('GET','http://devendra14.imad.hasura-app.io/counter', true);
+   request.send(null);     
 };
+
+   
+    
+    
+    
+    
+
+               
+
+
+
+
+
+
+
+
 //submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
